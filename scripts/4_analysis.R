ba23e78c4d95bb73b38a7e5ddd259baa0ba2a471
Alexandra Chang <changay@purdue.edu>
2025-06-16 22:05:58 -0400

    update

--- scripts/4_analysis.R
+++ scripts/4_analysis.R
@@ -11,7 +11,8 @@ setwd("/Users/al8xi8/Documents/GitHub/lcrisks_simulation")
 library(readr)
 library(dplyr)
 library(tidyr)
-
+install.packages("janitor")
+library(janitor)
 
 # --- Load *No Screening* Data ---
 noscrn_path <- "results/output_noscrn/"
@@ -26,7 +27,22 @@ noscrn <- bind_rows(
     df <- read_csv(file, col_names = FALSE)
     df$file_id <- basename(file)  # optional: keep filename
     return(df)}))
+  
+  # Core variable names
+  noscrn_core <- c(
+  "Sex", "Lung Cancer Age", "Lung Cancer Death Age",
+  "Other Cause Mortatlity Age", "Overall Death Age",
+  "Histology", "Stage", "Treatment Burden", "LC risk at death")
 
+  # Followed by PST, PKY, YSQ
+  pst <- paste0("PST ", 1:6)
+  pky <- paste0("PKY ", 0:100)
+  ysq <- paste0("YSQ ", 0:100)
+
+  noscrn_colnames <- c(noscrn_core, pst, pky, ysq)
+  colnames(noscrn) <- noscrn_colnames
+
+  
 # --- Load *Screening* Data ---
 scrn_path <- "results/output_scrn/"
 
@@ -41,6 +57,15 @@ scrn <- bind_rows(
     df$file_id <- basename(file)
     return(df)}))
 
+  # Core variable names
+  scrn_colnames <- c(
+  "Sex", "Lung Cancer Age", "Lung Cancer Death Age", "Other Cause Mortatlity Age",
+  "Overall Death Age", "Histology", "Stage", "Place Holder", "Treatment Burden",
+  "Eligible", "Overdiagnosis", "Detected", "Survival Time",
+  "Diagnostic Death from Screening", "Quit Smoking",
+  "Eligible for Cessation Program", "Entry Age")
+
+  colnames(scrn)[1:17] <- scrn_colnames
 
 --------------------------------------------------------------------------------
 
